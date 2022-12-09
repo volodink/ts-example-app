254bd110149156db8d7c3000c8c465d679a97900
Konstantin Volodin <volodin.konstantin@gmail.com>
2022-12-09 20:51:50 +0000

    Пример с применением наследования и абстрактных классов

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,56 @@
-function greet(name: string): void {
-    console.log(`Hello ${name}`);
+class Vertex2d {
+    private _x: number;
+    private _y: number;
+
+    constructor(
+        public x: number,
+        public y: number,
+    ) {
+        this._x = x;      
+        this._y = y;      
+    }
+
+    public get x(): number {
+        return this._x;
+    }
+
+    public set x(n: number) {
+        this._x = n;
+    }
+
+    public get y(): number {
+        return this._y;
+    }
+
+    public set y(n: number) {
+        this._y = n;
+    }
+}
+class Vertex3d extends Vertex2d {
+    private _z: number;
+
+    public get z(): number {
+        return this._z;
+    }
+
+    public set z(n: number) {
+        this._z = n;
+    }
+
+    constructor(
+        public x: number,
+        public y: number,
+        public z: number
+    ) {
+        super(x, y);
+        this._z = z;      
+    }
+
+    public toString = () : string => {
+        return `(${this.x}, ${this.y}, ${this.z})`
+    }    
 }
 
-greet("rwerwerw");
-greet("asdasdas");
+let v3d: Vertex3d = new Vertex3d(10, 10, 10);
+
+console.log(v3d);

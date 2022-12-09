35f3a89c6fc9ee34ebf496a7cd4e25dcc296e0c9
Konstantin Volodin <volodin.konstantin@gmail.com>
2022-12-09 20:57:00 +0000

    Пример с применением наследования и абстрактных классов

--- src/app.ts
+++ src/app.ts
@@ -1,56 +1,93 @@
-class Vertex2d {
-    private _x: number;
-    private _y: number;
+// class Vertex2d {
+//     private _x: number;
+//     private _y: number;
 
-    constructor(
-        public x: number,
-        public y: number,
-    ) {
-        this._x = x;      
-        this._y = y;      
+//     constructor(
+//         public x: number,
+//         public y: number,
+//     ) {
+//         this._x = x;      
+//         this._y = y;      
+//     }
+
+//     public get x(): number {
+//         return this._x;
+//     }
+
+//     public set x(n: number) {
+//         this._x = n;
+//     }
+
+//     public get y(): number {
+//         return this._y;
+//     }
+
+//     public set y(n: number) {
+//         this._y = n;
+//     }
+// }
+
+// class Vertex3d extends Vertex2d {
+//     private _z: number;
+
+//     public get z(): number {
+//         return this._z;
+//     }
+
+//     public set z(n: number) {
+//         this._z = n;
+//     }
+
+//     constructor(
+//         public x: number,
+//         public y: number,
+//         public z: number
+//     ) {
+//         super(x, y);
+//         this._z = z;      
+//     }
+
+//     public toString = () : string => {
+//         return `(${this.x}, ${this.y}, ${this.z})`
+//     }    
+// }
+
+// let v3d: Vertex3d = new Vertex3d(10, 10, 10);
+
+// console.log(v3d);
+
+abstract class Person {
+    name: string;
+    
+    constructor(name: string) {
+        this.name = name;
     }
 
-    public get x(): number {
-        return this._x;
+    display(): void{
+        console.log(this.name);
     }
 
-    public set x(n: number) {
-        this._x = n;
+    abstract find(string): Person;
+}
+
+class Employee extends Person { 
+    empCode: number;
+    
+    constructor(name: string, code: number) { 
+        super(name); // must call super()
+        this.empCode = code;
     }
 
-    public get y(): number {
-        return this._y;
-    }
-
-    public set y(n: number) {
-        this._y = n;
+    find(name:string): Person { 
+        // execute AJAX request to find an employee from a db
+        return new Employee(name, 1);
     }
 }
-class Vertex3d extends Vertex2d {
-    private _z: number;
 
-    public get z(): number {
-        return this._z;
-    }
+let emp: Person = new Employee("James", 100);
+emp.display(); //James
 
-    public set z(n: number) {
-        this._z = n;
-    }
+let emp2: Person = emp.find('Steve');
 
-    constructor(
-        public x: number,
-        public y: number,
-        public z: number
-    ) {
-        super(x, y);
-        this._z = z;      
-    }
-
-    public toString = () : string => {
-        return `(${this.x}, ${this.y}, ${this.z})`
-    }    
-}
-
-let v3d: Vertex3d = new Vertex3d(10, 10, 10);
-
-console.log(v3d);
+console.log(emp);
+console.log(emp2);

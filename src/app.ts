b841b944bfe882da6d4ad790a0f9b05ff24c8f9d
Konstantin Volodin <volodin.konstantin@gmail.com>
2022-12-16 20:34:03 +0000

    Interface example

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,51 @@
-function greet(name: string): void {
-    console.log(`Hello ${name}`);
+interface Shape {
+    area(): number;
 }
 
-greet("rwerwerw");
-greet("asdasdas");
+class Rectangle implements Shape {
+    private width!: number;
+    private height!: number;
+    constructor (w:number, h:number) {
+        this.width = w;
+        this.height = h;
+    }
+    public area(): number {
+        return this.width * this.height;
+    }
+}
+
+class Triangle implements Shape {
+    private height!: number;
+    private base!: number;
+    constructor (b:number, h:number) {
+        this.base = b;
+        this.height = h;
+    }
+    public area(): number {
+        return (1/2) * this.base * this.height;
+    }
+}
+
+class Circle implements Shape {
+    private diameter!: number;
+    constructor (d: number) {
+        this.diameter = d;
+    }
+    public area(): number {
+        return Math.PI * this.diameter;
+    }
+}
+
+class Application {
+    public run() {
+        let rect: Rectangle = new Rectangle(10, 10);
+        console.log(rect.area());
+        let trig: Triangle = new Triangle(10, 10);
+        console.log(trig.area());
+        let circle: Circle = new Circle(10);
+        console.log(circle.area());
+    }
+}
+
+const app: Application = new Application();
+app.run();
